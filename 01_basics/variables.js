bc83a8559de9930be3c4cc58cd34ba547af91eb2
Ankita Nag <ankeetaa3@gmail.com>
2025-07-06 16:13:34 +0530

    minor changes

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,9 +1,9 @@
 const accountId=14323  //unchangeable 
-let accountm="anki56@gmail.com" 
+let accountm="anki56" 
 var sample="hellohibye"
 accountCity="Mumbai"
  let accountState //undefined
-accountm="annn90@gmail.com"
+accountm="annn"
 sample="kohkkk"
 accountCity="Delhi"
 console.table([accountId,accountm,sample,accountCity,accountState])
